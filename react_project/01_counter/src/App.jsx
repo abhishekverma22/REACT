0d59b9f0705700048b8aa456008f8f924e77412d
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-03-31 20:45:19 +0530

    explain useState

--- react_project/01_counter/src/App.jsx
+++ react_project/01_counter/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   }
   const subValue = ()=>{
     if(count>0){
-      updatedCount(--count)
+      updatedCount( --count)
     }
   }
 
@@ -25,7 +25,12 @@ function App() {
      <br/> <br />
      <button onClick={subValue}>Decrease</button>
 
+     <p>Learn React with Hitesh Sir |  <br /> <br />Create count app with Authentication max count 20 & min count 0</p>
 
+    <footer> Day 2 of learing react | hooks - UseState
+      
+  <p>useState is a function take default value useState(0) and return array [defaultValue, setCounter]  <br /> <br /> setCounter() function inside ++count & -- count </p>    
+   </footer>
     </>
   )
 }
